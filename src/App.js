d2b88890aeeeac594ad0cdefa929d2d4b0d67513
neoz666 <neoz.blockchain@gmail.com>
2023-12-21 11:59:32 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import Neo4jComponent from './Neo4jComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <Neo4jComponent />
+    </div>
+  );
+}
+
+export default App;
